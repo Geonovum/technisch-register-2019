5a23cb57cda7e3a1ebc88272553ebed14a1b68d3
thijsbrentjens <thijs@brentjensgeoict.nl>
2020-11-09 16:09:17 +0100

    img in repos.json and some comment cleanup index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-// TODO: how to deal with staging and production environment? Have a separate config for this?
+// TODO: document config
+// how to deal with staging and production environment? Have a separate config for this?
 include './autodeploy/registerConfig.php';
 $urlparts = explode("/", $_GET['url']);
 $cluster = False;
@@ -26,14 +27,13 @@ if (count($urlparts) > 0 ) {
       }
   }
 
-  // Process information from repos
+  // Use the length of the cluster array (excluding index.html)
   $reposJson = file_get_contents($reposURL);
   $reposArr = json_decode($reposJson, true);
   $repoData = False;
   $models = [];
   // TODO: make more robust
   foreach ($reposArr as $re) {
-      // if the id is the same as the repo, the information is not a cluster, but we got the repo data directly.
       if ($re["id"]==$repo) {
         $repoData = $re;
         $indexPage = False;
@@ -43,10 +43,12 @@ if (count($urlparts) > 0 ) {
         array_push($models, $re);
       }
   }
+
   if (count($models) > 0 && $repoData == False) {
     // no repos, but models: we are in a clusterPage
     $clusterPage = True;
   }
+  // find all directories for a repo?
 }
 
 ?><html lang="nl">
@@ -57,7 +59,6 @@ if (count($urlparts) > 0 ) {
   </title>
   <link href="./resources/css/style.css" rel="stylesheet" type="text/css"/>
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"/>
-  </script>
  </head>
  <body>
   <header>
@@ -111,7 +112,8 @@ if (count($urlparts) > 0 ) {
        Ingang: informatiemodel
       </h3>
     <?php
-    // if we are building a cluster page, find the matching clusterinfo
+    // if a cluster page, find the matching clusterinfo
+    // if the general index, list all clusters
       if ($clusterPage) {
         // only display relevant models with the proper URL
         foreach ($models as $model) {
@@ -122,7 +124,7 @@ if (count($urlparts) > 0 ) {
              <span style="margin-left: 25px">
             <?php
               echo '<a href="./'.$cluster.'/'.$model["id"].'">';
-              echo $model["titel_kort"].'</a>' ; // e.g. BRT
+              echo $model["titel_kort"].'</a>' ; //BRT
               ?>
              </span>
             </p>
@@ -134,7 +136,7 @@ if (count($urlparts) > 0 ) {
             <?php
         }
       } else {
-        // if we are building the general index, display all clusters, with a URL to an index.html page
+        // display all clusters, with a URL to a index.html page
         $clustersJson = file_get_contents($clustersURL);
         $clustersArr = json_decode($clustersJson, true);
         foreach ($clustersArr as $cluster) {
@@ -165,8 +167,7 @@ if (count($urlparts) > 0 ) {
       ?>
     </div>
     <?php } else {
-      // This means this is not a cluster page and not the index page
-      // So we are in a repo page, for a specific model. Build the information for this
+      // If not, we are in a repo page, for a specific model
       ?>
       <div>
       <h2>
@@ -177,6 +178,7 @@ if (count($urlparts) > 0 ) {
       </p>
          </div>
          <div id="container">
+           <!-- TODO: find corresponding dirs, and match descriptions. Create URLs based on clusterid  -->
           <div id="leftcolumn">
             <?php
               include "listDescriptions.php";
